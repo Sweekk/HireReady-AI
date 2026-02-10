5fe8385dd9ab39e7d54d62e4218fb78298de3a89
Sweekk <sweekarshetty06@gmail.com>
2026-02-10 23:45:31 +0530

    signup page conflict resolved

--- app/(auth)/signup/page.js
+++ app/(auth)/signup/page.js
@@ -1,78 +1,5 @@
-<<<<<<< HEAD
-// "use client";
-// import { createAccount } from "../authlogic";
-// import { useState } from "react";
-// import { googlelogin } from "../authlogic";
-
-// export default function Login() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   return (
-//     <div className="bg-gray-100 flex flex-col items-center justify-center min-h-screen">
-//       <h2 className="text-3xl text-amber-950 mb-4 font-semibold">
-//         Job Seeker Portal
-//       </h2>
-//       <div className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-sm">
-//         <h2 className="text-2xl font-bold text-center mb-6">Sign up for HireReady</h2>
-
-//         <div className="mb-4">
-//           <label className="block text-gray-700 mb-1 text-sm">Email</label>
-//           <input
-//             type="email"
-//             placeholder="Enter your email"
-//             className="w-full px-4 py-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
-//             onChange={(event) => setEmail(event.target.value)}
-//           />
-//         </div>
-
-//         <div className="mb-6">
-//           <label className="block text-gray-700 mb-1 text-sm">Password</label>
-//           <input
-//             type="password"
-//             placeholder="Enter your password"
-//             className="w-full px-4 py-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
-//             onChange={(event) => setPassword(event.target.value)}
-//           />
-//         </div>
-
-//         <button
-//           className="w-full bg-blue-600 text-white py-2 mt-5 rounded-xl hover:bg-blue-700 transition-all"
-//           onClick={async (event) => {
-//             await createAccount(email, password);
-//           }}
-//         >
-//           Sigh up
-//         </button>
-//         <h2 className="flex font-semibold mt-2.5 justify-center ">OR</h2>
-//         <button
-//           onClick={async (event) => {
-//             await googlelogin(email);
-//           }}
-//           className="border p-2 hover:text-blue-600 rounded-lg flex items-center gap-2 w-full justify-center mt-3"
-//         >
-//           <img
-//             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRwSSm1FIX8PTMhol9LHlJ7nGjsPDl0b2MIA&s"
-//             className="w-5 h-5"
-//           />
-//           Sign up with Google
-//         </button>
-
-//         <p className="text-center text-sm text-gray-600 mt-4">
-//           Already have an account?
-//           <a className="text-blue-600 hover:underline" href="login">
-//             Login
-//           </a>
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
 "use client";
 
-=======
-"use client";
-
->>>>>>> 962f8a33040d0d0a1bf43689f4a9822b336280c1
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { createAccount, googlelogin } from "../authlogic";
@@ -94,9 +21,7 @@ export default function Signup() {
         </h2>
 
         <div className="mb-4">
-          <label className="block text-gray-700 mb-1 text-sm">
-            Email
-          </label>
+          <label className="block text-gray-700 mb-1 text-sm">Email</label>
           <input
             type="email"
             className="w-full px-4 py-2 border rounded-xl"
@@ -105,9 +30,7 @@ export default function Signup() {
         </div>
 
         <div className="mb-6">
-          <label className="block text-gray-700 mb-1 text-sm">
-            Password
-          </label>
+          <label className="block text-gray-700 mb-1 text-sm">Password</label>
           <input
             type="password"
             className="w-full px-4 py-2 border rounded-xl"
@@ -127,9 +50,7 @@ export default function Signup() {
           Sign Up
         </button>
 
-        <h2 className="flex font-semibold mt-2.5 justify-center">
-          OR
-        </h2>
+        <h2 className="flex font-semibold mt-2.5 justify-center">OR</h2>
 
         <button
           onClick={async () => {
